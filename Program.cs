a5c0ef2235935e29da01c8a69722d3e723bbd5fe
Taylo <Taylo@MARIEL>
2025-05-13 15:28:26 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace AssignmentThree
+{
+    class Program
+    {
+        static void MoveZeroes(int[] nums)
+        {
+            int nonZeroIndex = 0;
+            // moving non zeroes forward
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[nonZeroIndex] = nums[i];
+                    if (i != nonZeroIndex)
+                    {
+                        nums[i] = 0;
+                    }
+                    nonZeroIndex++;
+                }
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(" Please input nums: ");
+            string input = Console.ReadLine();
+
+            int[] nums = Array.ConvertAll(input.Split(','), int.Parse);
+
+            MoveZeroes(nums);
+
+            Console.WriteLine("Output: [" + string.Join(", ", nums) + "]");
+
+        }
+    }
+}
